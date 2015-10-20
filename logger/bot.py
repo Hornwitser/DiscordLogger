78906e7d3cf0fc9025833c164e453e18c8bcc09b
Hornwitser <discord@hornwitser.no>
2015-10-20 22:42:44 +0200

    Add basic command system
    Add simple command system with permissions and dynamic configuration.

--- logger/bot.py
+++ logger/bot.py
@@ -1,14 +1,23 @@
 import json
 import logging
 
-import discord
+from discord import Client, utils
 import pymysql
 
 
-class LoggerBot(discord.Client):
+def command(func):
+    func.command = None
+    return func
+
+
+class LoggerBot(Client):
     def __init__(self, config):
         Client.__init__(self)
         self.config = config
+        self.commands = []
+        for k, v in LoggerBot.__dict__.items():
+            if hasattr(v, 'command'):
+                self.commands.append(k)
 
     def on_socket_raw_send(self, msg, binary):
         self.log_msg(True, msg)
@@ -54,6 +63,263 @@ class LoggerBot(discord.Client):
                 cursor.execute(sql, (dr, raw))
 
         connection.commit()
+
+    def get_role(self, member):
+        if member.id in self.config['masters']:
+            return 'master'
+        elif member.id in self.config['admins']:
+            return 'admin'
+        elif any((r.id in self.config['admin_roles'] for r in member.roles)):
+            return 'admin'
+        elif member.id in self.config['ignores']:
+            return 'ignore'
+        else:
+            return 'user'
+
+    def on_message(self, msg):
+        if msg.channel.is_private: return
+        if not msg.content.startswith(self.config['trigger']): return
+
+        line = msg.content[len(self.config['trigger']):]
+        if ' ' in line:
+            cmd, arg = line.split(' ', 1)
+        else:
+            cmd, arg = line, None
+
+        if cmd not in self.commands: return
+        func = getattr(self, cmd)
+
+        role = self.get_role(msg.author)
+        if role == 'master':
+            func(msg, arg)
+
+        elif msg.channel.server.id not in config['active_servers']:
+            return
+
+        elif (role == 'admin' and cmd in self.config['admin_commands']
+              or role == 'user' and cmd in self.config['user_commands']):
+            func(msg, arg)
+
+        elif role != 'ignore' and self.config['noisy_deny']:
+            self.send_message(msg.channel, "You do not have permission to "
+                              "use this command.")
+
+    @command
+    def join(self, message, argument):
+        """{invite} - Ask LoggerBot to accept an invite."""
+        if argument is not None:
+            if 'http' not in argument:
+                argument = 'http://discord.gg/{}'.format(argument)
+            if self.accept_invite(argument):
+                self.send_message(message.channel, "Joined server.")
+            else:
+                self.send_message(message.channel, "Failed to accept invite.")
+        else:
+            self.send_message(message.channel, "Need invite to join")
+
+    @command
+    def leave(self, message, argument):
+        """[id] - Leave a server by id. Uses this server if id is not given."""
+        if argument is None:
+            server_id = message.channel.server.id
+        else:
+            server_id = argument
+
+        if server_id not in config['protected_servers']:
+            server = utils.find(lambda s: s.id == server_id, self.servers)
+            if server is not None:
+                self.leave_server(server)
+            else:
+                self.send_message(message.channel, "Can't find server with id "
+                                  "{}".format(server_id))
+        else:
+            self.send_message(message.channel,
+                              "Refusing to leave protected server")
+
+    @command
+    def help(self, message, argument):
+        """- Show this help text."""
+        role = self.get_role(message.author)
+        if role == 'master':
+            commands = self.commands
+        elif role == 'admin':
+            commands = self.config['admin_commands']
+        elif role == 'user':
+            commands = self.config['user_commands']
+
+        text = "Available commands:\n"
+        for command in sorted(commands):
+            text += "{} {}\n".format(command, getattr(self, command).__doc__)
+        self.send_message(message.channel, text)
+
+
+    def add_field(self, field, field_type, channel, argument):
+        if argument is None:
+            self.send_message(channel, "Error: missing argument")
+
+        elif field_type == 'user':
+            users = [u.id for u in argument]
+            if len(users):
+                self.config[field].update(users)
+                names = ', '.join([u.name for u in argument])
+                self.send_message(channel, "Added users {}.".format(names))
+            else:
+                self.send_message(channel, "No users mentioned to add.")
+
+        elif field_type == 'command':
+            commands = set(argument.split(' ')).intersection(self.commands)
+            if len(commands):
+                self.config[field].update(commands)
+                cmds = ", ".join(commands)
+                self.send_message(channel, "Added commands {}".format(cmds))
+            else:
+                self.send_message(channel, "No matching commands to add.")
+
+        elif field_type == 'role':
+            roles = channel.server.roles
+            name = argument.lower()
+            matching_roles = [r for r in roles if name in r.name.lower()]
+            if len(matching_roles) == 1:
+                self.config[field].update([matching_roles[0].id])
+                name = matching_roles[0].name
+                self.send_message(channel, "Added role {}.".format(name))
+            elif len(matching_roles) == 0:
+                self.send_message(channel, "No roles matched {}.".format(name))
+            else:
+                names = ', '.join([r.name for r in matching_roles])
+                self.send_message(channel, "Which one? {}.".format(names))
+
+        elif field_type == 'server':
+            servers = self.servers
+            name = argument.lower()
+            matching_servers = [s for s in servers if name in s.name.lower()]
+            if len(matching_servers) == 1:
+                self.config[field].update([matching_servers[0].id])
+                name = matching_servers[0].name
+                self.send_message(channel, "Added server {}.".format(name))
+            elif len(matching_servers) == 0:
+                self.send_message(channel, "No server match {}.".format(name))
+            else:
+                names = ', '.join([r.name for r in matching_servers])
+                self.send_message(channel, "Which one? {}.".format(names))
+
+    def remove_field(self, field, field_type, channel, argument):
+        if argument is None:
+            self.send_message(channel, "Error: missing argument")
+
+        elif field_type == 'user':
+            users = [u.id for u in argument]
+            if len(users):
+                self.config[field].difference_update(users)
+                names = ', '.join([u.name for u in argument])
+                self.send_message(channel, "Removed users {}.".format(names))
+            else:
+                self.send_message(channel, "No users mentioned to remove.")
+
+        elif field_type == 'command':
+            commands = set(argument.split(' ')).intersection(self.commands)
+            if len(commands):
+                self.config[field].difference_update(commands)
+                cmds = ", ".join(commands)
+                self.send_message(channel, "Removed commands {}.".format(cmds))
+            else:
+                self.send_message(channel, "No matching commands to remove.")
+
+        elif field_type == 'role':
+            roles = channel.server.roles
+            name = argument.lower()
+            matching_roles = [r for r in roles if name in r.name.lower()]
+            if len(matching_roles) == 1:
+                self.config[field].difference_update([matching_roles[0].id])
+                name = matching_roles[0].name
+                self.send_message(channel, "Removed role {}.".format(name))
+            elif len(matching_roles) == 0:
+                self.send_message(channel, "No roles matched {}.".format(name))
+            else:
+                names = ', '.join([r.name for r in matching_roles])
+                self.send_message(channel, "Which one? {}".format(names))
+
+        elif field_type == 'server':
+            servers = self.servers
+            name = argument.lower()
+            matching_servers = [s for s in servers if name in s.name.lower()]
+            if len(matching_servers) == 1:
+                self.config[field].difference_update([matching_servers[0].id])
+                name = matching_servers[0].name
+                self.send_message(channel, "Removed server {}.".format(name))
+            elif len(matching_servers) == 0:
+                self.send_message(channel, "No server match {}.".format(name))
+            else:
+                names = ', '.join([r.name for r in matching_servers])
+                self.send_message(channel, "Which one? {}.".format(names))
+
+    @command
+    def add_admin(self, message, argument):
+        """{user} ... - Add mentioned users to list of admins."""
+        self.add_field('admins', 'user', message.channel, message.mentions)
+
+    @command
+    def remove_admin(self, message, argument):
+        """{user} ... - Remove mentioned users from list of admins."""
+        self.remove_field('admins', 'user', message.channel, message.mentions)
+
+    @command
+    def add_admin_role(self, message, argument):
+        """{role} - Add role to admin role list."""
+        self.add_field('admin_roles', 'role', message.channel, argument)
+
+    @command
+    def remove_admin_role(self, message, argument):
+        """{role} - Remove role from admin role list."""
+        self.remove_field('admin_roles', 'role', message.channel, argument)
+
+    @command
+    def add_user_command(self, message, argument):
+        """{command} ... - Add command(s) to user command list."""
+        self.add_field('user_commands', 'command', message.channel, argument)
+
+    @command
+    def remove_user_command(self, message, argument):
+        """{command} ... - Remove command(s) from user command list."""
+        self.remove_field('user_commands', 'command',
+                          message.channel, argument)
+
+    @command
+    def add_admin_command(self, message, argument):
+        """{command} ... - Add command(s) to admin command list."""
+        self.add_field('admin_commands', 'command', message.channel, argument)
+
+    @command
+    def remove_admin_command(self, message, argument):
+        """{command} ... - Remove command(s) from admin command list."""
+        self.remove_field('admin_commands', 'command',
+                          message.channel, argument)
+
+    @command
+    def listen_on(self, message, argument):
+        """{server name} - Start listening for commands on server"""
+        self.add_field('active_servers', 'server', message.channel, argument)
+
+    @command
+    def ignore_server(self, message, argument):
+        """{server name} - Stop listening for commands on server"""
+        self.remove_field('active_servers', 'server',
+                          message.channel, argument)
+
+
+    @command
+    def debug_conf(self, message, argument):
+        lines = ['    {!r}: {!r},'.format(k, self.config[k]) for k in sorted(self.config)]
+        print('\n'.join(['# LoggerBot config', '{']+lines+['}']))
+
+    @command
+    def debug(self, message, argument):
+        """{python expression} - Evaluate an arbitrary python expression"""
+        try:
+            self.send_message(message.channel, eval(argument))
+        except Exception as e:
+            self.send_message(message.channel,
+                              '{} {}'.format(type(e).__name__, e))
 
 
 def write_config(config):
--- logger/config-example.py
+++ logger/config-example.py
@@ -1,13 +1,33 @@
 # Logger bot configuration.
 {
+    # These messages will dissapear after the bot has been run
+    # Most of these settings can be changed from within the bot
+    # itself.  See the help command.
+    'active_servers': set(),
+    'admin_commands': {'help', 'ignore_server', 'listen_on', 'leave', 'join'},
+    'admin_roles': set(),
+    'admins': set(),
+
+    # Discord user for the bot.
+    'bot_user': 'logger@example.com',
+    'bot_password': 'Password for Discord user'
+
     # MySQL database connection.
     'db_host': 'localhost',
     'db_user': 'logger',
     'db_password': 'Password for database user',
     'db_schema': 'discord',
 
-    # Discord user for the bot.
-    'bot_user': 'logger@example.com',
-    'bot_password': 'Password for Discord user'
+    'ignores': set(),
+
+    # Set of user ids that are masters of bot, and can do any command.
+    'masters': {'your-user-id-number'},
+    'noisy_deny': True,
+    'protected_servers': set(),
+
+    # Character used to triggering commands in the bot.  Setting it
+    # to '!', means commands start with an ! character (e.g, !help).
+    'trigger': '!',
+
+    'user_commands': {'help', 'leave', 'join'},
 }
-# vim: set ft=python
