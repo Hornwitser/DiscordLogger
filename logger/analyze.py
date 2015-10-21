24fd60398f0ed0a4ca3647bdef46ba654d9536aa
Hornwitser <discord@hornwitser.no>
2015-10-21 18:54:30 +0200

    Fix config name change breakages
    Alter the name for the config in logger/analyze.py and logger/.gitignore
    to use the .py ending.  Missing from the config file move in 24a1ffc4.

--- logger/analyze.py
+++ logger/analyze.py
@@ -402,7 +402,7 @@ def defaultdict_factory():
 
 if __name__ == '__main__':
     global connection
-    config = eval(open('config').read())
+    config = eval(open('config.py').read())
     logging.basicConfig(level=logging.INFO)
     connection = pymysql.connect(host=config['db_host'],
                                  user=config['db_user'],
